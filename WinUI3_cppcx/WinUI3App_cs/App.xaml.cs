15a1a601378a07b3346651808d7869a1e59836ce
Robson <rp.pontin@gmail.com>
2023-03-23 16:04:51 -0700

    Updated sample with application/window shutdown logic

--- WinUI3_cppcx/WinUI3App_cs/App.xaml.cs
+++ WinUI3_cppcx/WinUI3App_cs/App.xaml.cs
@@ -1,23 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI.Dispatching;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -29,6 +16,10 @@ namespace WinUI3App_cs
     /// </summary>
     public partial class App : Application
     {
+        private Window m_window;
+        private AppWindow m_appWindow;
+        private DispatcherQueue m_dispatcherQueue;
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -45,9 +36,77 @@ namespace WinUI3App_cs
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
+            m_window.Closed += Window_Closed;
+
+            m_dispatcherQueue = m_window.DispatcherQueue;
+            m_dispatcherQueue.ShutdownStarting += DispatcherQueue_ShutdownStarting;
+            m_dispatcherQueue.ShutdownCompleted += DispatcherQueue_ShutdownCompleted;
+
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(m_window);
+            // Retrieve the WindowId that corresponds to hWnd.
+            Microsoft.UI.WindowId windowId =
+                Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+
+            m_appWindow = AppWindow.GetFromWindowId(windowId);
+            m_appWindow.Closing += AppWindow_Closing;
+            m_appWindow.Destroying += AppWindow_Destroying;
+
             m_window.Activate();
         }
 
-        private Window m_window;
+        /*
+         * A. Shutting down with Window.Close()
+         *
+         * 1. Window.Close()
+         * 2. Window.Closed 
+         * 3. AppWindow.Destroying
+         * 4. Process terminated.
+         * 
+         * B. Shutting down with AppWindow.Destroy() and then Window.Close()
+         * 
+         * 1. AppWindow.Destroy()
+         * 2. AppWindow.Destroying
+         * 3. Window.Close() - called inside the Destroying event handler
+         * 4. Window.Closed
+         * 5. AppWindow.Destroying
+         * 6. Process terminated.
+         * 
+         * C. Shutting down with Window.Close() and AppWindow.Destroy()
+         * 
+         * 1. Window.Close()
+         * 2. Window.Closed
+         * 3. AppWindow.Destroying
+         * 4. AppWindow.Destroy() - called inside the AppWindow.Destroying event handler
+         * 5. AppWindow.Destroying... - this event gets called a few times in a loop until process terminates.
+         * 6. Process NOT terminated.
+         * 
+         * NOTE: calling AppWindow.Destroy() inside Window.Closed event does not cause 
+         * multiple triggers of AppWindow.Destroying, but the process still get's stuck and never terminated.
+         */
+
+        private void AppWindow_Destroying(AppWindow sender, object args)
+        {
+            Debug.WriteLine("[Debug] AppWindow_Destroying()");
+        }
+
+        private void AppWindow_Closing(AppWindow sender, AppWindowClosingEventArgs args)
+        {
+            Debug.WriteLine("[Debug] AppWindow_Closing()");
+        }
+
+        private void DispatcherQueue_ShutdownCompleted(DispatcherQueue sender, object args)
+        {
+            Debug.WriteLine("[Debug] DispatcherQueue_ShutdownCompleted()");
+        }
+
+        private void DispatcherQueue_ShutdownStarting(DispatcherQueue sender, DispatcherQueueShutdownStartingEventArgs args)
+        {
+            Debug.WriteLine("[Debug] DispatcherQueue_ShutdownStarting()");
+        }
+
+        private void Window_Closed(object sender, WindowEventArgs args)
+        {
+            Debug.WriteLine("[Debug] Window_Closed()");
+        }
     }
 }
--- WinUI3_cppcx/WinUI3App_cs/MainWindow.xaml.cs
+++ WinUI3_cppcx/WinUI3App_cs/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace WinUI3App_cs
 {
@@ -22,6 +25,50 @@ namespace WinUI3App_cs
             var str = test.GetStringValue;
 
             myButton.Content = "Clicked";
+        }
+          
+        private async void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            // Ways of terminating the process:
+            // 1. Window.Close()
+            // 2. AppWindow.Destroy()
+            int exitType = 0;
+
+            switch (exitType)
+            {
+                case 0:
+                    this.Close();
+                    break;
+
+                case 1:
+                    var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+                    // Retrieve the WindowId that corresponds to hWnd.
+                    Microsoft.UI.WindowId windowId =
+                        Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+
+                    var appWindow = AppWindow.GetFromWindowId(windowId);
+
+                    appWindow.Destroy();
+                    break;
+            }
+        }
+
+        int m_dispatcherRuns = 0;
+        private void btnTestDispatcher_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                this.DispatcherQueue.TryEnqueue(async () =>
+                {
+                    m_dispatcherRuns++;
+
+                    Random random = new();
+
+                    await Task.Delay(random.Next(1000));
+
+                    tbText.Text = "[Debug][Test] Running dispatcher #" + m_dispatcherRuns.ToString() + "...";                  
+                });
+            }
         }
     }
 }
