5013f9a8d1bd2311aa08b11c1ef945fec10b208c
Robson <rp.pontin@gmail.com>
2023-12-05 10:41:44 -0800

    added logic to get list of known devices from autoplayer WPD

--- AutoPlayListenerApp/AutoPlayApp/MainWindow.xaml.cpp
+++ AutoPlayListenerApp/AutoPlayApp/MainWindow.xaml.cpp
@@ -6,9 +6,15 @@
 #if __has_include("MainWindow.g.cpp")
 #include "MainWindow.g.cpp"
 #endif
+#include <iostream>
+#include <string>
+#include <Windows.h>
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+
+// A list of all known devices that have been connected to PC.
+#define AUTOPLAY_KNOWN_DEVICES_SUBKEY L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\AutoplayHandlers\\KnownDevices"
 
 namespace winrt::AutoPlayApp::implementation
 {
@@ -31,6 +37,42 @@ namespace winrt::AutoPlayApp::implementation
 
     void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
     {
-        // NOT implemented
+        HKEY hKey = nullptr;
+        DWORD retCode = ERROR_SUCCESS;
+
+        auto result = RegOpenKeyEx(
+            HKEY_CURRENT_USER,
+            AUTOPLAY_KNOWN_DEVICES_SUBKEY,
+            0,
+            KEY_READ,
+            &hKey);
+
+        if (result != ERROR_SUCCESS)
+        {
+            return;
+        }
+
+        std::vector<std::wstring> kownDevices;
+        DWORD i = 0;
+
+        while (retCode == ERROR_SUCCESS)
+        {
+            wchar_t subKeyName[MAX_PATH];
+            DWORD cbName = MAX_PATH;
+            retCode = RegEnumKeyEx(
+                hKey,
+                i,
+                subKeyName,
+                &cbName,
+                nullptr,
+                nullptr,
+                nullptr,
+                nullptr);
+            if (retCode == ERROR_SUCCESS)
+            {
+                kownDevices.emplace_back(subKeyName);
+            }
+            ++i;
+        }
     }
 }
