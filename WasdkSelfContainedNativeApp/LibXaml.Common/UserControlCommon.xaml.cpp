8d622d5b3624800f1bf2c17c541d6e25b3b67cea
Robson <rp.pontin@gmail.com>
2024-01-23 09:03:28 -0800

    Implement WinAppSdk self-contained app with test libs

---
+++ WasdkSelfContainedNativeApp/LibXaml.Common/UserControlCommon.xaml.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "UserControlCommon.xaml.h"
+#if __has_include("UserControlCommon.g.cpp")
+#include "UserControlCommon.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::LibXaml::Common::implementation
+{
+    int32_t UserControlCommon::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void UserControlCommon::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void UserControlCommon::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
---
+++ WasdkSelfContainedNativeApp/LibXaml.Common/UserControlCommon.xaml.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "UserControlCommon.g.h"
+
+namespace winrt::LibXaml::Common::implementation
+{
+    struct UserControlCommon : UserControlCommonT<UserControlCommon>
+    {
+        UserControlCommon()
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::LibXaml::Common::factory_implementation
+{
+    struct UserControlCommon : UserControlCommonT<UserControlCommon, implementation::UserControlCommon>
+    {
+    };
+}
